fc44f83e3a42f8bc4a1ea4a8af6f551fb27cbebd
Alan Lucas <alucas@gmail.com>
2019-06-26 23:27:38 -0400

    Attempting to add Cucumber to the project.

---
+++ src/test/java/tests/PaymentTypeStepdefs.java
@@ -0,0 +1,19 @@
+package tests;
+
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+
+public class PaymentTypeStepdefs {
+  @Given("^Customer does not have an account wants to pay with PayPal$")
+  public void customerDoesNotHaveAnAccountWantsToPayWithPayPal() {
+  }
+
+  @When("^Customer enters required info and clicks PayPal button$")
+  public void customerEntersRequiredInfoAndClicksPayPalButton() {
+  }
+
+  @Then("^Customer is redirected to PayPal site to complete purchase$")
+  public void customerIsRedirectedToPayPalSiteToCompletePurchase() {
+  }
+}
